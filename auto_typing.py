d17a4dda82cb68af920da33e50abacd778f944a4
Sabuj <sabujrouth2000@gmail.com>
2022-10-19 11:02:12 +0530

    triangles draw using py

--- auto_typing.py
+++ auto_typing.py
@@ -3,7 +3,7 @@ import pyautogui
 import time
 time.sleep(5)
 count = 0
-while count <= 50:
-    pyautogui.typewrite("Hello World!")
+while count <= 30:
+    pyautogui.typewrite("hey")
     pyautogui.press("Enter")
     count = count+1
