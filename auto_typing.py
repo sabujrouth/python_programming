0f314100d21aecde4fc34886f3c05c0c600dad99
Sabuj <sabujrouth2000@gmail.com>
2022-10-21 10:39:33 +0530

    something old

--- auto_typing.py
+++ auto_typing.py
@@ -3,7 +3,7 @@ import pyautogui
 import time
 time.sleep(5)
 count = 0
-while count <= 30:
-    pyautogui.typewrite("hey")
+while count <= 50:
+    pyautogui.typewrite("Hello World!")
     pyautogui.press("Enter")
     count = count+1
