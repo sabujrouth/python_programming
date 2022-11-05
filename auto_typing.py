e10bf00ab5dac8b70c3ff9ef3f3413c19cfdbd10
Sabuj <sabujrouth2000@gmail.com>
2022-11-05 15:00:43 +0530

    calculator

--- auto_typing.py
+++ auto_typing.py
@@ -4,6 +4,6 @@ import time
 time.sleep(5)
 count = 0
 while count <= 100:
-    pyautogui.typewrite("Gurudev")
+    pyautogui.typewrite("Heheheheheheheheheh")
     pyautogui.press("Enter")
     count = count+1
